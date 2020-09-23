bb6be7a7e9c90c2c125a8879f72ee6c5301ae49a
Dayvid Castro <wellescastro@gmail.com>
2020-09-23 10:08:46 -0300

    refactored preprocessing and demo img output added

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,13 +1,26 @@
 import re
-# Script to filter portuguese samples for simplifying the dataset
-data_basepath = "DSL-Task/data/DSLCC-v2.1"
-# 1. Read the dataset
-for partition in ["train/task1-train.txt", "train/task1-dev.txt", "gold/A.txt"]:
-    with open(f"{data_basepath}/{partition}") as f:
-        text_data = f.read()
-        portuguese_texts = re.findall(".+\tpt-[B P][R T]\n", text_data)
-        print("Preprocessing {} portugues samples for partition {}".format(len(portuguese_texts), partition))
-    
-    new_partition = partition.replace(".txt", "_pt.txt")
-    with open(f"{data_basepath}/{new_partition}", "w") as f:
-        f.writelines(portuguese_texts)
+import json
+
+def filter_portuguese(text):
+    portuguese_texts = re.findall(".+\tpt-[B P][R T]\n", text)
+    return portuguese_texts
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="DSL-Task/data/DSLCC-v2.1", help="Directory which contains the 'train' and 'gold' folders")
+
+    args = parser.parse_args()
+
+    basepath = args.data_dir
+
+    # 1. Read the dataset
+    for partition in ["train/task1-train.txt", "train/task1-dev.txt", "gold/A.txt"]:
+        with open(f"{basepath}/{partition}") as f:
+            text_data = f.read()
+            text_data = filter_portuguese(text_data)
+
+        new_partition = partition.replace(".txt", "_pt.txt")
+        with open(f"{basepath}/{new_partition}", "w") as f:
+            f.writelines(text_data)
